e6f44e8c8811dbdebdd6ae72b438d1b01de88cb5
Gavin Stapleton <gav.stapleton@gmail.com>
2021-08-25 09:40:05 +1000

    typo in svelte config again

--- svelte.config.js
+++ svelte.config.js
@@ -1,6 +1,6 @@
 /** @type {import('@sveltejs/kit').Config} */
 
-import adapter from '@svelte/adapter-netlify';
+import adapter from '@sveltejs/adapter-netlify';
 const config = {
 	kit: {
 		// hydrate the <div id="svelte"> element in src/app.html
